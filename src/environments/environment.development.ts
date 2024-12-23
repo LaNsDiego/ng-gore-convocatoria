042db7e7497435530680252e332e1dfe79473701
Diego Rolando Layme Valeriano <vandiego_pandax@hotmail.com>
2024-12-22 21:46:07 -0500

    cambios

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   apiUrl : 'http://localhost:8000/api',
   storageUrl : 'http://localhost:8000/storage',
-  mapboxKey : 'pk.eyJ1IjoiZHZhbnNsIiwiYSI6ImNseGtudzIwMjAzZjIyam9yZnZqaHRza3cifQ.z-xRdaLgI_r5ozMjXB1ynA',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   // storageUrl : 'https://convocatoriaback.regiontacna.gob.pe/api/storage',
-  apiUrl : 'http://localhost:8000/api',
-  storageUrl : 'http://localhost:8000/api/storage',
-  mapboxKey : 'pk.eyJ1IjoiZHZhbnNsIiwiYSI6ImNseGtudzIwMjAzZjIyam9yZnZqaHRza3cifQ.z-xRdaLgI_r5ozMjXB1ynA',
+  apiUrl : 'https://convocatoriaback.regiontacna.gob.pe/api',
+  storageUrl : 'https://convocatoriaback.regiontacna.gob.pe/api/storage',
 };
